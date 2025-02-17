7cd64afada81a83c39c8612e542575d11aece46b
BrunoFerrada <brunoferrada1212@gmail.com>
2025-02-17 00:48:11 -0300

    detalles

--- src/app/modules/dashboard/components/nav-menu/nav-menu.component.ts
+++ src/app/modules/dashboard/components/nav-menu/nav-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-menu',
@@ -11,7 +12,16 @@ export class NavMenuComponent {
   linkItems: { label: string, routerLink: string } [] = [
     { label: 'Inicio', routerLink: 'home' },
     { label: 'Estudiantes', routerLink: 'students' },
-    { label: 'Cursos', routerLink: 'courses' }
+    { label: 'Cursos', routerLink: 'courses' },
   ]
 
+  constructor(private router: Router) {
+
+  }
+
+  logOut(): void {
+    localStorage.removeItem('token');
+
+    this.router.navigate(['auth', 'login'])
+  }
 }
